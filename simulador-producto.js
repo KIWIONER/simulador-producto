3c8c61f376b0ff22be178b99bb8fdb3a13a49604
matiasidiart <matiasidiartviera@gmail.com>
2022-11-28 21:14:50 +0100

    simulador acabado

--- simulador-producto.js
+++ simulador-producto.js
@@ -5,13 +5,16 @@ const comprarProductos = () => {
     let precio = 0;
     let compraTotal = 0;
     let algoMas = false;
+    let conTarjeta = true;
+    let pagarConTarjeta = 0;
+    let confirmarTarjeta = 0;
+   // let datosTarjeta= 0;
+    //let confirmarTarjeta = pagarConTarjeta;
 
     do {
         producto = prompt('que quieres comprar? Tenemos sudaderas, camisetas y gorras')
         cantidad = parseInt(prompt ('¿Cuantas unidades quieres llevarte?'))
 
-        let cantidadValidada = validarCantidad(cantidad);
-        
     switch (producto) {
         case "sudaderas":
                 precio = 69;
@@ -27,39 +30,52 @@ const comprarProductos = () => {
             precio = 0;
             cantidad = 0;
     }
-    compraTotal += precio * cantidadValidada;
+    compraTotal += precio * cantidad
+    alert ('el total son '+compraTotal+'€')
     
     algoMas = confirm ('¿Quieres añadir algo más?');
-
+    
     } while (algoMas)
     
-    const totalConDescuento = aplicarDescuento(compraTotal);
-    const totalConEnvio = calcularEnvio(totalConDescuento);
-
-    return totalConEnvio;
-}
-
-function validarCantidad(cantidad) {
-    while (Number.isNaN(cantidad) || cantidad === 0) {
-        if (cantidad !== 0) {
-            alert('Debe escribir un número.');
-        } else {
-            alert('Agrega la cantidad de productos que quieres.');
+    conTarjeta = confirm ('Quieres pagar con tarjeta?');
+        
+    do{
+        pagarConTarjeta = parseInt(prompt('Añada el número de su tarjeta'));
+        confirmarTarjeta = parseInt(prompt('Confirme el número de su tarjeta'));;  
+        
+        if (pagarConTarjeta === confirmarTarjeta){
+                alert ('el numero es correcto, hemos deducido de su cuenta el importe de'+compraTotal+'€.')
+                }else{ 
+                    alert ('El numero es incorrecto')        
         }
-        cantidad = parseInt(prompt('¿Cuántas unidades quieres llevarte?'));
+        conTarjeta = confirm ('Deseas comprar algo más?');
+            if (conTarjeta){
+                do {
+                    producto = prompt('que quieres comprar? Tenemos sudaderas, camisetas y gorras')
+                    cantidad = parseInt(prompt ('¿Cuantas unidades quieres llevarte?'))
+            
+                switch (producto) {
+                    case "sudaderas":
+                            precio = 69;
+                            break;
+                    case "camisetas":
+                            precio = 24;
+                            break;
+                    case "gorras":
+                            precio = 15;
+                            break;
+                    default:
+                        alert ('Algo no ha ido bien, vuelve a elegir qúe producto qieres');
+                        precio = 0;
+                        cantidad = 0;
+                }
+                compraTotal += precio * cantidad
+                alert ('el total son '+compraTotal+'€')
+                
+                algoMas = confirm ('¿Quieres añadir algo más?');
+                
+                } while (algoMas)
+        }
+        }while (conTarjeta)
     }
-    return cantidad;
-}
-
-//aplicamos un descuento
-
-const aplicarDescuento = (compraTotal) => {
-    let totalDescuento = 0;
-
-    if(compraTotal >= 20){
-        totalDescuento = compraTotal * 0.2;
-        return totalDescuento;
-    }else{
-        return compraTotal;
-    }
-}
+comprarProductos()
---
+++ simuladorConFunciones.js
@@ -0,0 +1,142 @@
+// EJEMPLO DESAFÍO ENTREGABLE
+
+const comprarProductos = () => {
+    let producto = '';
+    let cantidad = 0;
+    let precio = 0;
+    let totalCompra = 0;
+    let seguirComprando = false;
+
+    do {
+        /**
+         * Le damos al usuario la posibilidad de agregar solo 3 productos, shampoo,
+         * acondicionador o ambos.
+         */
+        producto = prompt ("¿Querés comprar shampoo, acondicionador o ambos?", "Ej: ambos");
+        cantidad = parseInt(prompt ("¿Cuántos querés comprar?"));
+
+        let cantidadValidada = validarCantidad(cantidad);
+
+    switch (producto) {
+        case "shampoo":
+            precio = 500;
+            break;
+        case "acondicionador":
+            precio = 700;
+            break;
+        case "ambos":
+            precio = 1100;
+            break;
+        default:
+            alert("Alguno de los datos ingresados no es correcto");
+            precio= 0;
+            cantidad= 0;
+    }
+
+    totalCompra += precio * cantidadValidada;
+
+    // El método confirm retornara true o false dependiendo si el usuario acepta o cancela la acción
+    seguirComprando = confirm("¿Querés agregar otro producto?");
+    } while (seguirComprando)
+    
+
+    const totalConDescuento = aplicarDescuento(totalCompra);
+    const totalConEnvio = calcularEnvio(totalConDescuento);
+
+    return totalConEnvio;
+}
+
+
+const validarCantidad = (cantidad) => {
+    while (Number.isNaN(cantidad) || cantidad === 0) {
+        if (cantidad !== 0) {
+            alert('Debe agregar un número.')
+        } else {
+            alert('Debe agregar una cantidad de producto.')
+        }
+        cantidad = parseInt(prompt("¿Cuántos querés comprar?"))
+    }
+
+    return cantidad;
+};
+
+// Función para aplicar un descuento del 20% al total de la compra si esta es mayor a 5000
+const aplicarDescuento = (totalCompra) => {
+    let totalConDescuento = 0;
+
+    if (totalCompra >= 5000) {
+        totalConDescuento = totalCompra * 0.80;
+        return totalConDescuento;
+    } else {
+        return totalCompra;
+    }
+}
+
+// Función para calcular el envío según el total de la compra
+const calcularEnvio = (totalCompra) => {
+    let tieneEnvioADomicilio = false;
+
+    tieneEnvioADomicilio = confirm("¿Querés envío a domicilio?");
+
+    if (tieneEnvioADomicilio && totalCompra >= 2000) {
+    alert("Tenés envio gratis. El total de tu compra es $" + totalCompra);
+    } else if (tieneEnvioADomicilio && totalCompra < 2000 && totalCompra !== 0) {
+    totalCompra += 700;
+    alert("El envío cuesta $700. El total de tu compra es $" + totalCompra);
+    } else {
+    alert("El total de tu compra es $" + totalCompra);
+    }
+
+    return totalCompra;
+};
+
+const calcularCantidadDeCuotas = () => {
+    let cuotas = 0;
+    let tieneCuotas = false;
+
+    tieneCuotas = confirm('¿Queres pagar en cuotas?');
+
+    if (tieneCuotas) {
+        cuotas = parseInt(prompt('¿En cuantas cuotas queres pagar?'));
+        if (cuotas === 0) {
+            cuotas = 1;
+        } else if (Number.isNaN(cuotas)) {
+            calcularCantidadDeCuotas();
+        }
+    } else {
+        cuotas = 1
+    }
+
+    return cuotas;
+};
+
+const calcularIntereses = (cuotas) => {
+    let tasa = 12.3;
+    let sinIntereses = 0;
+    let tasaTotal = 0;
+    let interesesTotales = 0;
+
+    if (cuotas === 1) {
+        return sinIntereses;
+    } else {
+        tasaTotal = tasa + cuotas * 0.2;
+        interesesTotales = tasaTotal * cuotas;
+        return interesesTotales;
+    }
+};
+
+const calcularTotalAPagar = (totalCompra, cuotas, intereses) => {
+    totalCompra = (totalCompra + intereses);
+    let valorCuota = totalCompra / cuotas;
+    alert('El total a pagar es $'+totalCompra+' en '+cuotas+' cuotas de $'+valorCuota.toFixed(2))
+};
+
+const totalCompra = comprarProductos();
+const cuotas = calcularCantidadDeCuotas();
+const intereses = calcularIntereses(cuotas);
+
+calcularTotalAPagar(totalCompra, cuotas, intereses);
+
+
+// Invocamos a las funciones
+// calcularEnvio(aplicarDescuento(comprarProductos()));
